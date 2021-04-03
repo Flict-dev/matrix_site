800a85ce875f9373e770a74b6e4232127b3d7295
Max <zhdanom.maksi@gmail.com>
2021-04-03 13:37:12 +0300

    security commit

--- Martix/settings.py
+++ Martix/settings.py
@@ -55,12 +55,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Martix.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
